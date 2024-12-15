15d318f6a080ad32e6361d7840c56660881f7f97
Rana Hamdy <104355652+ranahamdy1@users.noreply.github.com>
2024-12-15 11:24:47 +0200

    add contact us alert

--- lib/features/home/main_navigation/main_navigation_screen.dart
+++ lib/features/home/main_navigation/main_navigation_screen.dart
@@ -1,3 +1,4 @@
+import 'package:abo_maged_app/core/widgets/text_widget.dart';
 import 'package:abo_maged_app/features/home/basket/view/basket_screen.dart';
 import 'package:abo_maged_app/features/home/main_navigation/widget/selection_icon_widget.dart';
 import 'package:abo_maged_app/features/home/home/view/home_screen.dart';
@@ -6,6 +7,7 @@ import 'package:abo_maged_app/features/home/profile/view/profile_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:abo_maged_app/core/utils/app_assets.dart';
 import 'package:abo_maged_app/core/utils/app_colors.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class MainNavigationScreen extends StatefulWidget {
@@ -16,6 +18,40 @@ class MainNavigationScreen extends StatefulWidget {
 
 class _NavigationExampleState extends State<MainNavigationScreen> {
   int currentPageIndex = 0;
+
+  void _showBottomSheet(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          height: 223.h,
+          width: 427.w,
+          decoration: const BoxDecoration(
+            color: AppColors.secondColor,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(22.0),
+              topRight: Radius.circular(22.0),
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Image.asset("line".getPngAsset),
+              const TextWidget("قم بالتواصل معنا عبر الآتي"),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image.asset("phone-call 1".getPngAsset),
+                  Image.asset("telegram".getPngAsset),
+                  Image.asset("whatsapp".getPngAsset),
+                ],
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -41,6 +77,11 @@ class _NavigationExampleState extends State<MainNavigationScreen> {
                     setState(() {
                       currentPageIndex = index;
                     });
+
+                    // Show bottom sheet when the "Orders" destination is selected (index 2)
+                    if (index == 3) {
+                      _showBottomSheet(context);
+                    }
                   },
                   selectedIndex: currentPageIndex,
                   destinations: <Widget>[
